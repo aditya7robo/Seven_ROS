da9acce70221e329693f85fb17f019b79054818f
Tal-seven <mohammedtalha89@gmail.com>
2019-02-09 23:25:36 +0530

    first package commit

---
+++ src/imu_ros_uavcan/seven_msgs/build/catkin_generated/installspace/seven_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
+
+_prepend_path("${seven_msgs_DIR}/.." "msg" seven_msgs_MSG_INCLUDE_DIRS UNIQUE)
+set(seven_msgs_MSG_DEPENDENCIES std_msgs)
---
+++ src/imu_ros_uavcan/seven_msgs/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/talha/Seven_Robotics/imu_ros_uavcan/seven_msgs/build/devel:$CMAKE_PREFIX_PATH"
+export PKG_CONFIG_PATH="/home/talha/ros_ax2550/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/lib/pkgconfig"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/talha/Seven_Robotics/imu_ros_uavcan/seven_msgs/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/talha/Seven_Robotics/imu_ros_uavcan/seven_msgs:$ROS_PACKAGE_PATH"
