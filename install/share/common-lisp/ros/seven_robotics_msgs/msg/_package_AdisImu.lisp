de517ffec1553bbd48aae2a296948883676eda35
Tal-seven <mtalha@sevenrobotics.in>
2019-11-29 20:52:05 +0530

    ignore install dir

--- install/share/common-lisp/ros/seven_robotics_msgs/msg/_package_AdisImu.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package seven_robotics_msgs-msg)
-(cl:export '(HEADER-VAL
-          HEADER
-          GYRO_LATEST-VAL
-          GYRO_LATEST
-          ACCEL_LATEST-VAL
-          ACCEL_LATEST
-))
--- install/share/uavcan_ros_bridge/cmake/uavcan_ros_bridge-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
-
-_prepend_path("${uavcan_ros_bridge_DIR}/.." "" uavcan_ros_bridge_MSG_INCLUDE_DIRS UNIQUE)
-set(uavcan_ros_bridge_MSG_DEPENDENCIES std_msgs;sensor_msgs)
