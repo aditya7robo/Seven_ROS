e0e5b8e7add4141b2f3a08d226fa7ca5addfc3ce
Tal-seven <mtalha@sevenrobotics.in>
2020-06-21 22:03:21 +0530

    env loader script for remote launch

---
+++ env_loader_sevenPC1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export ROS_WS=/home/talha/SevenRobotics/Seven_ROS
+source $ROS_WS/devel/setup.bash
+export PATH=$ROS_ROOT/bin:$PATH
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
+export ROS_MASTER_URI=http://sevenRobot1:11311
+export ROS_HOSTNAME=192.168.0.4
+
+exec "$@"
