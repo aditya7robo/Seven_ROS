86a90a887c760bacf9b953de6d52e61991a72e1e
Mohammed Talha Arif <mtalha@sevenrobotics.in>
2022-09-10 17:10:25 +0530

    udoo ros distro updated to melodic

--- env_loader_udooqdl.sh
+++ env_loader_udooqdl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 export ROS_ROOT=/opt/ros
-export ROS_DISTRO=kinetic
-export ROS_PACKAGE_PATH=/opt/ros/kinetic/share
-export PATH=$PATH:/opt/ros/kinetic/bin
-export LD_LIBRARY_PATH=/opt/ros/kinetic/lib
-export PYTHONPATH=/opt/ros/kinetic/lib/python2.7/site-packages
+export ROS_DISTRO=melodic
+export ROS_PACKAGE_PATH=/opt/ros/melodic/share
+export PATH=$PATH:/opt/ros/melodic/bin
+export LD_LIBRARY_PATH=/opt/ros/melodic/lib
+export PYTHONPATH=/opt/ros/melodic/lib/python2.7/site-packages
 export ROS_MASTER_URI=http://sevenRobot1:11311
-export ROS_HOSTNAME=192.168.1.23
+export ROS_HOSTNAME=udooqdl
 export ROS_IP=192.168.1.23
-export CMAKE_PREFIX_PATH=/opt/ros/kinetic
-touch /opt/ros/kinetic/.catkinexport 
+export CMAKE_PREFIX_PATH=/opt/ros/melodic
+touch /opt/ros/melodic/.catkinexport 
 
 exec "$@"
