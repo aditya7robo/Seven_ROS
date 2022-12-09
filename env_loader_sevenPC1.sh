b86918a2a85eaebe0cf27d1e1b04e67cea05545b
seven-robot-2 <seven-robot-2@seven-robot-2.lan>
2022-12-09 22:11:53 +0530

    modified parameters specific to robot2

--- env_loader_sevenPC1.sh
+++ env_loader_sevenPC1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export ROS_WS=/home/talha/SevenRobotics/Seven_ROS
+export ROS_WS=/home/talha/Seven_ROS
 source $ROS_WS/devel/setup.bash
 export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
-export ROS_MASTER_URI=http://sevenRobot1:11311
+export ROS_MASTER_URI=http://sevenrobot2:11311
 export ROS_HOSTNAME=sevenPC1
 
 export DISPLAY=:0
